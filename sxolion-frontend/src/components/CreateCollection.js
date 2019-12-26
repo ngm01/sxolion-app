c8213557792d08f46831f9507bb89db87ed80bb8
ngm01 <nathaniel.g.moore@gmail.com>
2019-12-26 13:07:07 -0500

    collection creation

--- sxolion-frontend/src/components/CreateCollection.js
+++ sxolion-frontend/src/components/CreateCollection.js
@@ -4,40 +4,39 @@ class CreateCollection extends React.Component {
     constructor(props){
         super(props)
         this.state = {
-            title: "",
-            description: "",
-            canViewFlag: "PUBLIC",
-            items: []
+            collection: {
+                userId: this.props.userId,
+                title: "",
+                description: "",
+                canViewFlag: "PUBLIC",
+                items: []
+            }
         }
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    componentDidMount(){
-        console.log(this.props.userId)
-    }
-
     handleChange(e){
-        console.log(e.target.name);
-        switch(e.target.name){
-            case 'title':
-                this.setState({title: e.target.value})
-                break;
-            case 'description':
-                this.setState({description: e.target.value})
-                break;
-            case 'canViewFlag':
-                this.setState({canViewFlag: e.target.value})
-                break;
-            default:
-                console.log(e.target.name);
-        }
+        let collection = this.state.collection;
+        collection[e.target.name] = e.target.value;
+        this.setState({collection});
     }
 
     handleSubmit(e){
         e.preventDefault();
-        console.log(this.state)
+        console.log(this.state.collection)
+        fetch('http://localhost:5000/api/collections/', {
+            method: "POST",
+            body: JSON.stringify(this.state.collection),
+            headers: {
+                'Content-Type': 'application/json'
+              },
+        }).then(res => {
+            return res.json()
+        }).then(data => {
+            console.log(data);
+        })
     }
 
     render(){
@@ -49,18 +48,18 @@ class CreateCollection extends React.Component {
                     <label>
                         Title:
                         <input name="title" type="text" 
-                        value={this.state.title} 
+                        value={this.state.collection.title} 
                         onChange={this.handleChange} />
                     </label>
                     <label>
                         Description:
                         <textarea name='description' 
-                        value={this.state.description}
+                        value={this.state.collection.description}
                         onChange={this.handleChange} />
                     </label>
                     <label>
                         Access Priveleges:
-                        <select name="canViewFlag" value={this.state.canViewFlag}
+                        <select name="canViewFlag" value={this.state.collection.canViewFlag}
                         onChange={this.handleChange}>
                             <option value="PUBLIC">Public (Anyone can view)</option>
                             <option value="PRIVATE">Private (Only I can view)</option>
